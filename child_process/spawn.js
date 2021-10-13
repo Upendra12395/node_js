f1997ad204c87ab746e9f22a4486fcbdcedd34ea
Upendra12395 <ch95upendra@gmail.com>
2021-10-13 15:42:56 +0530

    spawn example

---
+++ child_process/spawn.js
@@ -0,0 +1,25 @@
+const {spawn} = require('child_process')
+const { stdout } = require('process')
+
+const child = spawn('ls', ['-lh'])
+
+child.stderr.on('stderr', (stderr)=>{
+    console.log(`stderr : ${stderr}`)
+})
+child.stdout.on('data', (data)=>{
+    console.log(`stdout : ${data}`)
+})
+
+child.on('error', (error)=>{
+    console.log(error)
+})
+
+child.on('exit', (code, signal)=>{
+    if(code){
+        console.log(code)
+    }
+    if(signal){
+        console.log(signal)
+    }
+    console.log("done")
+})
